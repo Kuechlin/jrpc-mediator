3731c85695c1c97769a75dd097127a4fe0ed8d88
Yannick Küchlin <y.kuechlin@Taifun-tofu.de>
2022-10-27 15:36:46 +0200

    fix: bundle includes peer deps

--- src/jrpc-mediator-core/vite.config.ts
+++ src/jrpc-mediator-core/vite.config.ts
@@ -1,16 +1,18 @@
 import { resolve } from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    plugins: [visualizer()],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib/index.ts'),
             name: '@jrpc-mediator/core',
-            fileName: 'jrpc-mediator-core',
+            fileName: (format) => `jrpc-mediator-core.${format}.js`,
         },
         rollupOptions: {
-            external: ['axios'],
+            external: ['axios', 'reflect-metadata'],
             output: {
                 globals: {
                     axios: 'axios',
--- src/jrpc-mediator-react/vite.config.ts
+++ src/jrpc-mediator-react/vite.config.ts
@@ -1,20 +1,22 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), visualizer()],
     build: {
         lib: {
             entry: resolve(__dirname, 'lib', 'index.tsx'),
             name: '@jrpc-mediator/react',
-            fileName: 'jrpc-mediator-react',
+            fileName: (format) => `jrpc-mediator-react.${format}.js`,
         },
         rollupOptions: {
             external: [
                 'react',
                 'react-dom',
+                'react/jsx-runtime',
                 'axios',
                 '@tanstack/react-query',
                 '@jrpc-mediator/core',
